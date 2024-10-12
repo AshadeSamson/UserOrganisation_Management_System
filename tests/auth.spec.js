cb25d8937a5d61d3dc68e252031f57fae36a4c2d
Ashade Samson <ashadesamson99@gmail.com>
2024-10-12 08:41:07 +0100

    wrote test for register and login endpoints

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,12 +1,12 @@
 import { app } from "../server";
 import  request  from "supertest";
 import { User, Organisation } from "../models/associations";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 import jwt from "jsonwebtoken";
 
 
 jest.mock("../models/associations");
-jest.mock("bcryptjs");
+jest.mock("bcryptjs/dist/bcrypt.js");
 jest.mock("jsonwebtoken");
 
 
@@ -22,4 +22,100 @@ describe("Auth Endpoints", () => {
       };
     
     const token = "jwtSampleToken"
+
+    beforeEach(() => {
+      bcrypt.hash.mockResolvedValue('hashed_password');
+      bcrypt.compare.mockResolvedValue(true);
+      jwt.sign.mockReturnValue(sampleToken);
+  
+      User.create.mockResolvedValue(sampleUser);
+      Organisation.create.mockResolvedValue({ name: "Ashade's Organisation", description: '' });
+      User.findOne.mockResolvedValue(sampleUser);
+    });
+
+
+    // register endpoint test
+    test("should register new user and return token", async () => {
+      const res = await request(app)
+      .post("/auth/register")
+      .send({
+        firstName: 'Ashade',
+        lastName: 'Samson',
+        email: 'ashon@example.com',
+        password: 'password123',
+        phone: '1234567890',
+      })
+
+
+      expect(res.statusCode).toEqual(201);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          status: 'success',
+          message: 'Registration successful',
+          data: {
+            accessToken: token,
+            user: expect.objectContaining({
+              userId: testUser.userId,
+              firstName: testUser.firstName,
+              lastName: testUser.lastName,
+              email: testUser.email,
+              phone: testUser.phone,
+            }),
+          },
+        })
+      )
+    })
+
+
+
+    // login endpoint test
+    test("should login user and return token", async () => {
+      const res = await request(app)
+      .post("/auth/login")
+      .send({
+         email: 'ashon@example.com',
+         password: 'password123',
+       })
+
+
+       expect(res.statusCode).toEqual(200);
+       expect(res.body).toEqual(
+        expect.objectContaining({
+          status: 'success',
+          message: 'Login successful',
+          data: {
+            accessToken: token,
+            user: expect.objectContaining({
+              userId: testUser.userId,
+              firstName: testUser.firstName,
+              lastName: testUser.lastName,
+              email: testUser.email,
+              phone: testUser.phone,
+            }),
+          },
+        })
+      )
+    })
+
+
+    // login failure test
+    test("shouldn't log user in and return error message", async () => {
+      bcrypt.compare.mockResolvedValue(false);
+      const res = await request(app)
+
+      .post("/auth/login")
+      .send({
+         email: 'wrongemail@example.com',
+         password: 'wrongpassword',
+       })
+
+       expect(res.statusCode).toEqual(401);
+       expect(res.body).toEqual(
+        expect.objectContaining({
+          status: 'Bad request',
+          message: 'Authentication failed',
+          statusCode: 401,
+        })
+       )
+    })
 })
