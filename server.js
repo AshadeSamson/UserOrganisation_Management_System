f09abb482ff169ef33039955dd5e732016b68896
Ashade Samson <ashadesamson99@gmail.com>
2024-10-24 09:10:31 +0100

    updated swagger ui config

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ const appInit = () => {
 
     // middlewares
     app.use(express.json())
-    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+    const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs, {
+        customCss:
+            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: CSS_URL,
+    }));
 
     // routes
     app.get("/", (req, res) => { res.json("you are welcome")})
