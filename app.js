519aaa8c7766506c204b4c200518ee116b526622
Ashade Samson <ashadesamson99@gmail.com>
2024-10-24 08:21:21 +0100

    adjusted entry file for remote server

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import { app } from "./server.js"
 import { PORT } from "./config/config.js"
 
-export const server = app.listen(PORT || 0, () => {
-    console.log(`Server running on port ${server.address().port}`)
-})
+// for localhost environment
+// export const server = app.listen(PORT || 0, () => {
+//     console.log(`Server running on port ${server.address().port}`)
+// })
+
+
+// for production environment (vercel)
+export default app;
 
 
 
