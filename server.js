82709eae320104c02122fbde27b4fa78f832a5f1
Ashade Samson <ashadesamson99@gmail.com>
2024-10-24 08:30:39 +0100

    adjusted server file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from "express"
-import { db } from "./config/db.js"
-import authRouter from "./routes/authRoute.js"
-import organisationRouter from "./routes/orgRoute.js"
-// import swaggerUI from "swagger-ui-express"
-// import swaggerDocs from "./swagger.json" assert { type: 'json' }
+import express from "express";
+import { db } from "./config/db.js";
+import authRouter from "./routes/authRoute.js";
+import organisationRouter from "./routes/orgRoute.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "./swagger.json" assert { type: 'json' };
 
 
 const appInit = () => {
@@ -17,7 +17,7 @@ const appInit = () => {
 
     // middlewares
     app.use(express.json())
-    // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
     // routes
     app.get("/", (req, res) => { res.json("you are welcome")})
